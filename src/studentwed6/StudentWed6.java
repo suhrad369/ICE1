c26ad88c3693b6082a3768dd1fca980841bbb441
WaterGitH <mehrdad.iravani@sheridancollege.ca>
2023-05-24 18:46:32 -0400

    This is the 1st  commit for StudentWed6 local repository

---
+++ src/studentwed6/StudentWed6.java
@@ -0,0 +1,35 @@
+package studentwed6;
+
+import java.util.Scanner;
+
+/**
+ * This class does this and that
+ * @author Mehrdad Iravani
+ */
+
+public class StudentWed6{
+    
+    public static void main(String[] args){
+        
+        Student[] students = new Student[2];
+        
+        Scanner input = new Scanner(System.in);
+        
+        for (int i=0; i < students.length; i++){
+            
+            System.out.println("Enter Student's name:");
+            String name;
+            name = input.nextLine();
+            
+            Student student = new Student(name);
+            
+            students[i] = student;
+        }
+        
+        System.out.println("Printing the students:");
+        
+        for(Student student: students){
+            System.out.println(student.getName());
+        }
+    }
+}
